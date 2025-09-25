57f558d427aae1aea14f3ce38bf9a2585ca2e996
Michael Jones <mjj4@njit.edu>
2025-09-25 19:01:02 -0400

    Added a second paddle and a way to control it

--- pong/scripts/game_world.gd
+++ pong/scripts/game_world.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 func _ready():
-	$paddle.position = Vector2(700,300)
+	var screen_size = get_tree().root.get_window().size
+	var init_pos = Vector2((screen_size.x * 0.833),(screen_size.y / 2))
+	$right_paddle.position = init_pos
+	init_pos = Vector2((screen_size.x * 0.166),(screen_size.y / 2))
+	$left_paddle.position = init_pos
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
-		$paddle.position += Vector2(0,-10)
+		$right_paddle.position += Vector2(0,-10)
 	if Input.is_action_pressed("ui_down"):
-		$paddle.position += Vector2(0,10)
+		$right_paddle.position += Vector2(0,10)
+	if Input.is_action_pressed("UP"):
+		$left_paddle.position += Vector2(0,-10)
+	if Input.is_action_pressed("DOWN"):
+		$left_paddle.position += Vector2(0,10)
 		
