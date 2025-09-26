da278f7806cf792e14790a82fe3091a1ee86f56f
Michael Jones <mjjones923@gmail.com>
2025-09-26 00:56:53 -0400

    gave the ball a random starting velocity

--- pong/scripts/game_world.gd
+++ pong/scripts/game_world.gd
@@ -8,3 +8,8 @@ func _ready():
 	$left_paddle.position = init_pos
 	$worldboundry/Bottom_CollisionShape2D.position = Vector2(0,screen_size.y)
 	$Ball.position = Vector2(screen_size.x/2,screen_size.y/2)
+	var direction = 1 if randi_range(0,1) else -1
+	$Ball.linear_velocity.x = 150 * direction
+	direction = 1 if randi_range(0,1) else -1
+	$Ball.linear_velocity.y = 110 * direction
+	
