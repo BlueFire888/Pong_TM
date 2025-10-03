919fd0b370a616e1fece818f08e88c58e80cca3e
Michael Jones <mjj4@njit.edu>
2025-10-03 17:33:26 -0400

    added ball physics for bouncing and speeding up over time

---
+++ pong/scripts/ball.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if Engine.get_physics_frames() % 60 == 0:
+		var x_dir = 1 if linear_velocity.x > 0 else -1
+		var y_dir = 1 if linear_velocity.y > 0 else -1
+		apply_impulse(Vector2(5*x_dir,5*y_dir))
--- pong/scripts/game_world.gd
+++ pong/scripts/game_world.gd
@@ -4,7 +4,10 @@ func _ready():
 	var screen_size = get_tree().root.get_window().size
 	var init_pos = Vector2((screen_size.x * 0.833),(screen_size.y / 2))
 	$right_paddle.position = init_pos
-	init_pos = Vector2((screen_size.x * 0.166),(screen_size.y / 2))
+	init_pos = Vector2((screen_size.x * 0.166),(screen_size.y / 2))	
 	$left_paddle.position = init_pos
 	$worldboundry/Bottom_CollisionShape2D.position = Vector2(0,screen_size.y)
 	$Ball.position = Vector2(screen_size.x/2,screen_size.y/2)
+	var x_dir = 1 if randi_range(0,1) else -1
+	var y_dir = 1 if randi_range(0,1) else -1
+	$Ball.apply_impulse(Vector2(100*x_dir,100*y_dir))
